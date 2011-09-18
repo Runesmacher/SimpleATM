ee56a51199c52ea4de9fc45a4aa2d0de4bb50969
Runesmacher <runesmacher@hotmail.com>
2011-09-18 12:51:48 +0200

    Fixed for minecraft 1.8

--- Runesmacher/SimpleATM/SimpleATMBlockListener.java
+++ Runesmacher/SimpleATM/SimpleATMBlockListener.java
@@ -4,6 +4,7 @@ import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Sign;
 import org.bukkit.event.block.BlockBreakEvent;
 
@@ -52,32 +53,36 @@ public class SimpleATMBlockListener extends BlockListener {
         }
     }
 
+    @Override
     public void onBlockBreak(final BlockBreakEvent event) {
         if (event.isCancelled()) {
             return;
         }
 
-        if (event.getBlock().getState() instanceof Sign) {
-            Sign sign = (Sign) event.getBlock().getState();
-            String lines[] = sign.getLines();
-            if (sign.getLine(1).contains("[ATM]") || sign.getLine(0).contains("[ATM]")) {
-                player = event.getPlayer();
-                if (plugin.permissionHandler != null && plugin.permissionHandler.has(player, "SimpleATM.remove")) {
-                    player.sendMessage(ChatColor.GREEN + "ATM removed");
-                } else if (plugin.permissionHandler == null && player.isOp()) {
-                    player.sendMessage(ChatColor.GREEN + "ATM removed");
-                } else {
-                    event.setCancelled(true);
-                    sign.setLine(0, lines[0]);
-                    sign.setLine(1, lines[1]);
-                    sign.setLine(2, lines[2]);
-                    sign.setLine(3, lines[3]);
-                    sign.update();
-                    player.sendMessage(ChatColor.RED + "You do not have permission to remove an ATM.");
-                }
+        if ((event.getBlock().getType() != Material.WALL_SIGN)) {
+            return;
+        }
+
+        Sign sign = (Sign) event.getBlock().getState();
+        String lines[] = sign.getLines();
+        if (sign.getLine(1).contains("[ATM]") || sign.getLine(0).contains("[ATM]")) {
+            player = event.getPlayer();
+            if (plugin.permissionHandler != null && plugin.permissionHandler.has(player, "SimpleATM.remove")) {
+                player.sendMessage(ChatColor.GREEN + "ATM removed");
+            } else if (plugin.permissionHandler == null && player.isOp()) {
+                player.sendMessage(ChatColor.GREEN + "ATM removed");
             } else {
-                return;
+                event.setCancelled(true);
+                sign.setLine(0, lines[0]);
+                sign.setLine(1, lines[1]);
+                sign.setLine(2, lines[2]);
+                sign.setLine(3, lines[3]);
+                sign.update();
+                player.sendMessage(ChatColor.RED + "You do not have permission to remove an ATM.");
+
             }
+        } else {
+            return;
         }
     }
 }
